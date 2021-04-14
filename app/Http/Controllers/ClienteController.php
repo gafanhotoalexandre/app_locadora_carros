00e89312615174f9b1f829dddb1bead0a3c56582
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2021-04-14 17:54:11 -0300

    Operações de CRUD desenvolvidas para a tabela Marca

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -15,16 +15,7 @@ class ClienteController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return ['vou mandar' => 'mandei!'];
     }
 
     /**
@@ -45,17 +36,6 @@ class ClienteController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Cliente $cliente)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cliente $cliente)
     {
         //
     }
