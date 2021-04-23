4f50a5c99710b6b8ebc154e2e9c479082c5a4a62
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2021-04-22 23:55:17 -0300

    Implementando método login de AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credenciais = $request->all(['email', 'password']);
+
+        // autenticação (email e senha)
+        $token = auth('api')->attempt($credenciais);
+
+        if ($token) {// Usuário autenticado com sucesso
+            return response()->json([
+                'token' => $token
+            ], 200);            
+        } else {// Erro de usuário ou senha
+            return response()->json([
+                'erro' => 'Usuário ou Senha inválido!'
+            ], 403);
+            
+            // erro 401 = unauthorized -> não autorizado
+            // erro 403 = forbidden -> proibido (login inválido)
+        }
+
+        // retornar um JWT (Json Web Token)
+        return 'login';
+    }
+
+    public function logout()
+    {
+        return 'logout';
+    }
+
+    public function refresh()
+    {
+        return 'refresh';
+    }
+
+    public function me()
+    {
+        return 'me';
+    }
+}
