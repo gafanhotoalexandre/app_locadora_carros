0429efa837bea2c82869b9a25cbf11b97e27cce3
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2021-06-17 00:09:33 -0300

    implementando persistência de dados via post

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -46,17 +46,18 @@
         <modal-component id="modalMarca" titulo="Adicionar Marca">
             <template v-slot:conteudo>
                 <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Opcional. Informe o nome da marca.">
-                    <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp">
+                    <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" v-model="nomeMarca">
                 </input-container-component>
-
+                {{ nomeMarca }}
                 <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG">
-                    <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp">
+                    <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp" @change="carregarImagem($event)">
                 </input-container-component>
+                {{ arquivoImagem }}
             </template>
 
             <template v-slot:rodape>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
             </template>
         </modal-component>        
 
@@ -64,5 +65,40 @@
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                urlBase: 'http://localhost:8000/api/v1/marca',
+                nomeMarca: '',
+                arquivoImagem: [],
+            }
+        },
+        methods: {
+            carregarImagem(e) {
+                this.arquivoImagem = e.target.files
+            },
+            salvar() {
+                console.log(this.nomeMarca, this.arquivoImagem[0])
 
+                let formData = new FormData()
+                formData.append('nome', this.nomeMarca)
+                formData.append('imagem', this.arquivoImagem[0])
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Accept': 'application/json',
+                    }
+                }
+
+                axios.post(this.urlBase, formData, config)
+                    .then(response => {
+                        console.log(response)
+                    })
+                    .catch(errors => {
+                        console.log(errors)
+                    })
+            }
+        },
+    }
 </script>
